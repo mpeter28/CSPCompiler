9d1443edde79de076bd4dd72bc39ad31ae3f52bc
mpeter28 <marshallpeters@ymail.com>
2014-11-15 14:31:53 -0500

    For Unit testing

---
+++ src/TestConstraint.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestConstraint implements Constraint {
+
+    public List<List<AtomicConstraint>> flatten() {
+        List<List<AtomicConstraint>> preBuiltList = new ArrayList<List<AtomicConstraint>>();
+
+        List<AtomicConstraint> andBlock = new ArrayList<AtomicConstraint>();
+        andBlock.add(new AtomicConstraint(true, "A=1"));
+        andBlock.add(new AtomicConstraint(false, "A=2"));
+        andBlock.add(new AtomicConstraint(true, "B=1"));
+        andBlock.add(new AtomicConstraint(false, "B=2"));
+
+        preBuiltList.add(andBlock);
+
+        List<AtomicConstraint> andBlockTwo = new ArrayList<AtomicConstraint>();
+        andBlockTwo.add(new AtomicConstraint(true, "C=1"));
+        andBlockTwo.add(new AtomicConstraint(false, "C=2"));
+        andBlockTwo.add(new AtomicConstraint(true, "C=1"));
+        andBlockTwo.add(new AtomicConstraint(false, "C=2"));
+
+        preBuiltList.add(andBlockTwo);
+
+        return preBuiltList;
+    }
+
+}
