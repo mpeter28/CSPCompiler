4c326277c19a2494eebb424b63bef2866656868f
mpeter28 <marshallpeters@ymail.com>
2014-11-15 14:01:51 -0500

    OrConstraint

--- src/OrConstraint.java
+++ src/OrConstraint.java
@@ -2,8 +2,20 @@ import java.util.List;
 
 public class OrConstraint implements Constraint {
 
+    private Constraint leftConstraint;
+    private Constraint rightConstraint;
+
+    public OrConstraint(Constraint leftConstraint, Constraint rightConstraint) {
+        this.leftConstraint = leftConstraint;
+        this.rightConstraint = rightConstraint;
+    }
+
     @Override
     public List<List<AtomicConstraint>> flatten() {
-        return null;
+        List<List<AtomicConstraint>> flattenLeft = leftConstraint.flatten();
+        List<List<AtomicConstraint>> flattenRight = rightConstraint.flatten();
+
+        flattenLeft.addAll(flattenRight);
+        return flattenLeft;
     }
 }
