73750812f46838ba3256d8632e29cda9086db991
mpeter28 <marshallpeters@ymail.com>
2014-11-15 14:00:58 -0500

    AtomicConstraint

--- src/AtomicConstraint.java
+++ src/AtomicConstraint.java
@@ -1,9 +1,34 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class AtomicConstraint implements Constraint {
+
+    private boolean negated;
+    private String clause;
+
+    public AtomicConstraint(boolean negated, String clause) {
+        this.negated = negated;
+        this.clause = clause;
+    }
+
+    public boolean isNegated() {
+        return negated;
+    }
+
+    public String getClause() {
+        return clause;
+    }
+
     @Override
     public List<List<AtomicConstraint>> flatten() {
-        return null;
+        List<List<AtomicConstraint>> baseOr = new ArrayList<List<AtomicConstraint>>();
 
+        ArrayList<AtomicConstraint> baseAnd = new ArrayList<AtomicConstraint>();
+        baseAnd.add(this);
+
+        baseOr.add(baseAnd);
+
+        return baseOr;
     }
+
 }
