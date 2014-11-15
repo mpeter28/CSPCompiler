e6fbd105c83e91455a514f55f3c21d7ab33b2234
m-dub <mwthomasjr@gmail.com>
2014-11-15 14:10:50 -0500

    Initial draft of the AndConstraint.java file.

--- src/AndConstraint.java
+++ src/AndConstraint.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class AndConstraint implements Constraint {
-
+	
+	private Constraint left;
+	private Constraint right;
+	
+	public AndConstraint( Constraint l, Constraint r ) {
+		this.left = l;
+		this.right = r;
+	}
+	
     @Override
     public List<List<AtomicConstraint>> flatten() {
-        return null;
+       List<List<AtomicConstraint>> leftResult = left.flatten();
+       List<List<AtomicConstraint>> rightResult = right.flatten();
+       
+       ArrayList<List<AtomicConstraint>> flattened = new ArrayList<List<AtomicConstraint>>();
+       
+       for( List<AtomicConstraint> list : leftResult ) {
+    	   for( List<AtomicConstraint> list2 : rightResult ) {
+    		   List<AtomicConstraint> r = new ArrayList<AtomicConstraint>();
+    		   r.addAll( list );
+    		   r.addAll( list2 );
+    		   flattened.add( r );
+    	   }
+       }
+       
+       return flattened;
     }
 }
